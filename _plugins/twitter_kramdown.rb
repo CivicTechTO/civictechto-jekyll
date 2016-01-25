0fe27bcb8b40157b4f9b12568211412701d250a0
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-01-25 01:47:32 -0800

    Add custom markdown converter that adds Twitter handle autolink.

---
+++ _plugins/twitter_kramdown.rb
@@ -0,0 +1,14 @@
+module Jekyll
+  module Converters
+    class Markdown
+      class TwitterKramdown < ::Jekyll::Converters::Markdown::KramdownParser
+
+        TWITTER_HANDLE_REGEX = /(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9]+)/
+
+        def convert(content)
+          super.gsub(TWITTER_HANDLE_REGEX, '<a href="https://twitter.com/\1">@\1</a>')
+        end
+      end
+    end
+  end
+end
