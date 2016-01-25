bf15058a1b50fff099cf7dff59752433f49922a2
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-01-25 03:23:13 -0800

    Fix warning.

--- _plugins/twitter_kramdown.rb
+++ _plugins/twitter_kramdown.rb
@@ -3,7 +3,7 @@ module Jekyll
     class Markdown
       class TwitterKramdown < ::Jekyll::Converters::Markdown::KramdownParser
 
-        TWITTER_HANDLE_REGEX = /(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9]+)/
+        TWITTER_HANDLE_REGEX = /(?<=^|(?<=[^a-zA-Z0-9\-_\.]))@([A-Za-z]+[A-Za-z0-9]+)/
 
         def convert(content)
           super.gsub(TWITTER_HANDLE_REGEX, '<a href="https://twitter.com/\1">@\1</a>')
